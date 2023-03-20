bdad8ccb6926228afbb8c06249e1ae693c5b383c
ashidiqidimas <ashidiqidimas@gmail.com>
2023-03-20 21:43:56 +0700

    Added kudos card

--- Koodos/KudosEditor/KudosCardEditorScreen.swift
+++ Koodos/KudosEditor/KudosCardEditorScreen.swift
@@ -37,7 +37,65 @@ struct KudosEditorScreen_Previews: PreviewProvider {
 
 class KudosEditorViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    /// Used to store the old location of an UILabel before become the first responder
+    /// so we can animate it back to its original position when we has finished editing.
+    private var oldLocation = CGPoint(x: 0, y: 0)
+    
+    /// The location when the drag started. It used for calculating the relative drag location when scaling
+    private var dragStartLocation = CGPoint(x: 0, y: 0)
+    
+    /// Old scale of the view that is being interacted.
+    private var oldScale: CGFloat = 1
+    
+    private var isKeyboardShown = false
+    
+    /// Returns true if the UILabel is just added to the view
+    private var isNewTextView = false
+    
+    private var isFromOutsideTrashView = true
+    
+    private var keyboardAnimationDuration: Double?
+    private var keyboardAnimationCurve: Int?
+    
+    // MARK: Views
+    
+    private var kudosCard: UIView = {
+        let kudosCard = UIView(frame: .zero)
+        kudosCard.layer.cornerRadius = 24
+        kudosCard.backgroundColor = .systemPink
+        kudosCard.clipsToBounds = true
+        
+        return kudosCard
+    }()
+    
+    // MARK: - Methods
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupSubviews()
+    }
+    
+    func setupSubviews() {
+        setupKudosCard()
+        
+        setupConstraints()
+    }
+    
+    func setupKudosCard() {
+        view.addSubview(kudosCard)
+        
+        let cardSize = CGSize(
+            width: view.frame.width,
+            height: view.frame.width * 16/9
+        )
+        kudosCard.frame.size = cardSize
+        
+    }
+    
+    func setupConstraints() {
         
     }
     
