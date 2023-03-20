6cdafa8fe5ecbdfbd48e52fbe084b23bcbcfbb58
ashidiqidimas <ashidiqidimas@gmail.com>
2023-03-20 21:19:18 +0700

    initial commit

---
+++ Koodos/KoodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoodosApp.swift
+//  Koodos
+//
+//  Created by Dimas on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct KoodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
